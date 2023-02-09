e48c88567ef40c3c8d135d6cec410eaf25f2e665
Adrien <39885692+1-irdA@users.noreply.github.com>
2023-02-09 16:32:20 +0100

    Add 12

---
+++ 12_most_used_hour.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+def get_most_used_hour(path: str):
+    df = pd.read_csv(path, sep=";", header=None, names=["tweet", "date"])
+
+    date_column = "date"
+
+    # Select only one column
+    column = df[date_column]
+
+    # Convert date to datetime and keep year
+    column = pd.to_datetime(column, dayfirst=True).dt.hour
+
+    # Add header on series
+    column = column.to_frame(date_column)
+
+    # Group by date and count occurrences of each date
+    grouped = df.groupby(column[date_column]).size().reset_index(name="count")
+    
+    max_occur = grouped["count"].max()
+    return grouped[grouped["count"] == max_occur]["date"].values[0]
+
+def main():
+    hour = get_most_used_hour("./data/trump_tweets.txt")
+    print(f"Donald Trump tweets the most at {hour} h.")
+
+main()
